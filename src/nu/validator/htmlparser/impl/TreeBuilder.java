2866a0906f3feae25d5ea77a4bcdff1b57ceb8f7
Michael[tm] Smith <mike@w3.org>
2020-08-14 18:35:55 +0900

    Ensure namespace-aware “clear the stack” handling
    This change ensures that for all cases with spec requirements in the
    form “clear the stack back to a foo context” — which involves checking
    for elements with particular names — we only look for elements in the
    HTML namespace, rather than additionally looking for elements which
    aren’t in the HTML namespace but that also have those particular names.
    
    Otherwise, without this change, we aren’t in conformance with the spec
    requirements, and we fail several cases in the html5lib-tests suite.
    
    Fixes https://github.com/validator/htmlparser/issues/33

--- src/nu/validator/htmlparser/impl/TreeBuilder.java
+++ src/nu/validator/htmlparser/impl/TreeBuilder.java
@@ -3907,8 +3907,9 @@ public abstract class TreeBuilder<T> implements TokenHandler,
 
     private int findLastInTableScopeOrRootTemplateTbodyTheadTfoot() {
         for (int i = currentPtr; i > 0; i--) {
-            if (stack[i].getGroup() == TreeBuilder.TBODY_OR_THEAD_OR_TFOOT ||
-                    stack[i].getGroup() == TreeBuilder.TEMPLATE) {
+            if (stack[i].ns == "http://www.w3.org/1999/xhtml"
+                    && (stack[i].getGroup() == TreeBuilder.TBODY_OR_THEAD_OR_TFOOT
+                            || stack[i].getGroup() == TreeBuilder.TEMPLATE)) {
                 return i;
             }
         }
@@ -4699,7 +4700,7 @@ public abstract class TreeBuilder<T> implements TokenHandler,
 
     private int findLastOrRoot(int group) {
         for (int i = currentPtr; i > 0; i--) {
-            if (stack[i].getGroup() == group) {
+            if (stack[i].ns == "http://www.w3.org/1999/xhtml" && stack[i].getGroup() == group) {
                 return i;
             }
         }
